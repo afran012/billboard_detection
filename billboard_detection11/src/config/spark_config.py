0705fe225d48b549e47f97309c243edf4f140f55
afran012 <afran.012@gmail.com>
2025-01-27 12:42:13 -0500

    Remove blank files and unused code from the billboard detection project

--- billboard_detection11/src/config/spark_config.py
+++
@@ -1,9 +0,0 @@
-from pyspark.sql import SparkSession
-
-def create_spark_session():
-    return SparkSession.builder \
-        .appName("BillboardDetection") \
-        .config("spark.executor.memory", "16g") \
-        .config("spark.driver.memory", "8g") \
-        .config("spark.executor.cores", "4") \
-        .getOrCreate()
